7c8bf8bdc3028c659b9b84feefb899b48bd2c1bb
John Mertz <jmertz1999@gmail.com>
2025-11-06 00:25:58 -0600

    Created template for remaining pages and navigation

---
+++ app/src/main/java/com/cs407/pinpoint/NavPages.kt
@@ -0,0 +1,50 @@
+package com.cs407.pinpoint
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cs407.pinpoint.ui.screens.HomePage
+import com.cs407.pinpoint.ui.screens.ItemPage
+import com.cs407.pinpoint.ui.screens.LandingPage
+import com.cs407.pinpoint.ui.screens.LoginPage
+import com.cs407.pinpoint.ui.screens.SettingsPage
+import com.cs407.pinpoint.ui.screens.SignUpPage
+import com.cs407.pinpoint.ui.screens.UploadPage
+import com.cs407.pinpoint.ui.screens.UserPage
+
+@Composable
+fun PinPointApp(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "item_page",
+    ) {
+        composable("landing") {
+            LandingPage()
+        }
+        composable("signup") {
+            SignUpPage()
+        }
+        composable("login") {
+            LoginPage()
+        }
+        composable("home") {
+            HomePage()
+        }
+        composable("item_page") {
+            ItemPage()
+        }
+        composable("user_page") {
+            UserPage()
+        }
+        composable("upload_page") {
+            UploadPage()
+        }
+        composable("settings") {
+            SettingsPage()
+        }
+    }
+}
--- app/src/main/java/com/cs407/pinpoint/ui/screens/LoginPage.kt
+++ app/src/main/java/com/cs407/pinpoint/ui/screens/LoginPage.kt
@@ -1,4 +1,4 @@
-package com.cs407.pinpoint.ui
+package com.cs407.pinpoint.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
